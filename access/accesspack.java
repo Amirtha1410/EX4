e91db9fa3baedcb8e580fede77b6fbebea7caa96
Amirtha1410 <68841447+Amirtha1410@users.noreply.github.com>
2020-08-19 16:18:44 +0530

    Add files via upload

---
+++ access/accesspack.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package access;
+import pack.*;
+import pack.sub.*;
+/**
+ *
+ * @author ELCOT
+ */
+public class accesspack {
+     public static void main(String[]args)
+             {
+                 first obj=new first();
+                 obj.display();
+                 subpm obj1=new subpm();
+                 obj1.display();
+             }
+}
